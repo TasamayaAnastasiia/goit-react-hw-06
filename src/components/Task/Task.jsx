7601d62c63bf99464076b016e9369c26ffb1b15d
TasamayaAnastasiia <144161343+TasamayaAnastasiia@users.noreply.github.com>
2024-04-26 16:37:07 +0200

    try

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,20 @@
 import css from '../Task/Task.module.css';
 
 export const Task = ({ tasks }) => {
+
+    const handleDelete = (e) => {
+        const taskDelete = e.currentTarget.getAttribute('data-del');
+        const updatedTasks = tasks.filter(task => task !== taskDelete);
+        return updatedTasks;
+    }
+
     return (
         tasks.length !== 1 && (
             <ul className={css.ul}>
                 {tasks.map(task => task !== '' && (
                     <div className={css.li}>
                         <li key={task}>{task}</li>
-                        <svg id="icon-delete" width ="32" height="32" viewBox="0 0 32 32">
+                        <svg id="icon-delete" width ="32" height="32" viewBox="0 0 32 32" data-del={task} onClick={handleDelete}>
                         <path stroke-width="0.88" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="miter" d="M22.956 11.309l-0.96 12.503c0 1.081-0.877 1.958-1.958 1.958v0h-8.075c-1.081 0-1.958-0.877-1.958-1.958v0l-0.96-12.503"></path>
                         <path stroke-width="0.88" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="miter" d="M19.229 10.051h-5.83"></path>
                         <path stroke-width="0.88" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="miter" d="M11.25 10.051h-4.146v-1.212c0-0.495 0.401-0.896 0.896-0.896v0h6.027c0-0.003 0-0.006 0-0.009 0-0.941 0.884-1.704 1.973-1.704s1.973 0.763 1.973 1.704c0 0.003 0 0.006 0 0.009h6.027c0.495 0 0.896 0.401 0.896 0.896v0 1.212h-3.412"></path>
